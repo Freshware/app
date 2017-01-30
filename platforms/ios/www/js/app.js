d88488a099cb70a737f62541ab6e559a54e1b65f
Seiya Japon <info@freshware.es>
2017-01-30 09:59:07 +0100

    Menú funcional en toda la app

--- platforms/ios/www/js/app.js
+++ platforms/ios/www/js/app.js
@@ -35,298 +35,209 @@ function init()
 }
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//The directory to store data
-var store;
-
-//Used for status updates
-var $status;
-
-//URL of our asset
-// var assetURL = "http://expresionotaku.com/wp-content/uploads/2016/11/manga-store.jpg";
-var assetURL = "http://freshware.es/pruebas/app/new_expresion_otaku.zip";
-var assetURL2 = "http://freshware.es/pruebas/app/new_expresion_otaku2.zip";
-var assetURL3 = "http://freshware.es/pruebas/app/new_expresion_otaku3.zip";
-var assetURL4 = "http://freshware.es/pruebas/app/new_expresion_otaku4.zip";
-
-//File name of our important data file we didn't ship with the app
-var fileName = "new_expresion_otaku3.zip";
-
-function startDownload(file) {
-	$status = document.querySelector("#status");
-
-	$status.innerHTML = "Checking for data file.";
-
-	// store = cordova.file.dataDirectory;
-	store = cordova.file.documentsDirectory;
-
-	// store2 = cordova.file.applicationDirectory + "prueba_archivos/";
-	// alert('Antes del resolveLocalFileSystemURL - linea 28');
-
-	//Check for the file. 
-	// La funcón se define así: window.resolveLocalFileSystemURL(url, successCallback, errorCallback);
-	// Así qu eusaremos el success para cuando tengamos un archivo y el error para descargarlo
-	window.resolveLocalFileSystemURL(store + fileName, appStart, downloadAsset);
-	// download(assetURL, store, fileName)
-
-
-
-	/**
-	 * Starter vars.
-	 */
-	var index = 0;
-	var i;
-
-	/**
-	 * Need cordova.file plugin.
-	 * $ cordova plugin add org.apache.cordova.file
-	 * 
-	 * To get all urls see https://github.com/apache/cordova-plugin-file/blob/master/README.md#where-to-store-files
-	 * 
-	 */
-	var localURLs    = [
-	  // cordova.file.cacheDirectory,
-	  // cordova.file.applicationDirectory,
-	  // cordova.file.applicationStorageDirectory,
-	  // cordova.file.dataDirectory,
-	  cordova.file.documentsDirectory,
-	  // cordova.file.externalApplicationStorageDirectory,
-	  // cordova.file.externalCacheDirectory,
-	  // cordova.file.externalRootDirectory,
-	  // cordova.file.externalDataDirectory,
-	  // cordova.file.sharedDirectory,
-	  // cordova.file.syncedDataDirectory
-	];
-
-	/**
-	 * Recursive function for file entry.
-	 */
-	var addFileEntry = function (entry) {
-	  var dirReader = entry.createReader();
-	  dirReader.readEntries(
-	    function (entries) {
-	      var fileStr = "";
-	      var i;
-	      for (i = 0; i < entries.length; i++) {
-	        if (entries[i].isDirectory === true) {
-	          // Recursive -- call back into this subdirectory
-	          addFileEntry(entries[i]);
-	        } else {
-	          alert('full path: ' + entries[i].fullPath);
-	          index++;
-
-	          entries[i].remove();
-	        }
-	      }
-	    },
-	    function (error) {
-	      alert("readEntries error: " + error.code);
-	    }
-	  );
-	};
-
-	/**
-	 * Directory error.
-	 */
-	var addError = function (error) {
-	  alert("getDirectory error: " + error.code);
-	};
-
-	/**
-	 * Loop through the array.
-	 */
-	for (i = 0; i < localURLs.length; i++) {
-	  if (localURLs[i] === null || localURLs[i].length === 0) {
-	    continue; // skip blank / non-existent paths for this platform
-	  }
-	  window.resolveLocalFileSystemURL(localURLs[i], addFileEntry, addError);
-	}
-}
-
-function downloadAsset() {
-	// alert('dentro del downloadAsset - linea 111')
-
-	
-	var url = "http://freshware.es/pruebas/app/prueba.json";
-	$.getJSON( url, {
-		// tags: "mount rainier",
-		// tagmode: "any",
-		format: "json"
+$(window).on('load', function() {
+	console.log('HOLA MUNDO!!')
+	$('.c-hamburger').on('click', function(){
+		if ($('#menu').length)
+		{
+			quitarMenu();
+		}
+		else
+		{
+			cargarMenu();
+		}
 	})
-	.done(function( data ) {
-		// alert('Hemos leído el json - linea 121')
+});
 
-		var images = data['folders']['images'];
-		var array_images = $.map(images, function(el) { return el });
 
-		descargarImagenes(array_images);
-
-		// $.each(images, function( index, value ) {
-		//     console.log(value['file']);
-		// 	alert('Estamos leyendo el contenido de la variable images - linea 128')
-		//     alert(value['file']);
-			
-		// 	fileTransfer[index] = new FileTransfer();
-
-		// 	fileTransfer[index].download(url, store + fileName, 
-		// 		function(entry) {
-		// 			alert("Ha funcionado y hemos descargado un archivo - Linea 135");
-		// 			appStart();
-		// 		}, 
-		// 		function(err) {
-		// 			alert("Error - Linea 139");
-		// 			// console.dir(err);
-		// 			document.getElementById("texto").innerHTML=err.target;
-		// 		});
-		// });
-	})
-	.fail(function( jqxhr, textStatus, error ) {
-	    var err = textStatus + ", " + error;
-	    console.log( "Request Failed: " + err );
-	    alert( "Ha ido muy mal la lectura del json: " + err );
-	});
-
-}
-
-function descargarImagenes(array_images)
+function cargarMenu()
 {
-	// alert('dentro de descargarImages - Linea 157')
-	if (! (array_images.length === 0))
-	{
-		// alert('array_images.length > 0 - Linea 160')
-		var fileTransfer = new FileTransfer();
+	$('body').append('<div id="menu">' +
+						'<div class="container text-center">' +
+						'</div>' +
+					 '</div>');
 
-		var storeComplete = store + array_images[0]['file'].split('/').pop();
+	var wait_menu = setInterval(function() {
+		if ($('#menu').length)
+		{
+			clearInterval(wait_menu);
 
-		fileTransfer.download(array_images[0]['file'], storeComplete, 
-			function(entry) {
-				// alert("Ha funcionado y hemos descargado un archivo - Linea 165");
-				// appStart();
+			$.getJSON('/../assets/content.json', function(data) {
+				// Después de cargar el JSON creamos un
+				// objeto que lo contenga. Le llamamos "items"
+				var items = [];
+				$.each( data, function(key, val) {
+					items[key] = val;
+				});
 
-				$('.contenedor-textos').prepend('<p class="text-warning">Array de contenido (num elements = ' + array_images.length + '):' + array_images + '</p>');
-				$('.contenedor-textos').prepend('<p class="text-warning">:' + array_images + '</p>');
+				// Cargamlos el contenido de la página que hay en el JSON
+				// dependiendo del idioma y la página.
+				var current_lang = localStorage.getItem("lang");
 
-				$('.contenedor-textos').prepend('<p class="text-success">Añadido archivo: ' + array_images[0]['file'] + ' en dirección: ' + storeComplete + '</p>');
-				$('.contenedor-textos').prepend('<p class="text-info">Array de contenido:\n' + array_images + '</p>');
-				$('.contenedor-textos').prepend('<img src="' + storeComplete + '" width="100%"  height="auto" >');
+				$.each(items.language, function(lang, language) {
+					if (lang == current_lang)
+					{
+						var seccion = language.index.seccion;
 
-				$('.contenedor-textos').prepend('<hr>');
+						$.each(seccion, function(indice, valor) {
+							$.each(valor, function(index, val) {
+								$('#menu .container').append('<div class="row menu_seccion' + (indice + 1) + '">' +
+																'<div class="col-xs-12">' +
+																	'<ul></ul>' +
+																'</div>' +
+															 '</div>');
 
-				array_images.shift();
+								if (index == 'menu')
+								{
+									$.each(val.links, function(key, value) {
+										$('.menu_seccion' + (indice + 1)).find('ul').append('<li><a href="/' + val.folder + '/' + key + '.html">' + value + '</a></li>');
+									})
+								}
+								else
+								{
+									$('.menu_seccion' + (indice + 1)).find('.col-xs-12').prepend('<h2><a href="#" class="menu_link_toggle">' + valor.h1 + '</a></h2>');
+								}
+							});
+						});
+					}
+				});
 
-				descargarImagenes(array_images);
-			}, 
-			function(err) {
-				alert("Error - Linea 173");
-				// console.dir(err);
-				document.getElementById("texto").innerHTML=err.target;
-			}
-		);
-	}
-	else
-	{
-		alert('fin de las descargas - Linea 180');
-	}
+				$('#menu').append('<div class="menu_footer">' +
+									'<div class="row">' +
+										'<div class="col-xs-4 text-left">' +
+											'<a href="' + window.location.protocol + '//' + window.location.host + '/' + '">HOME</a>' +
+										'</div>' +
+										'<div class="col-xs-4 text-center">' +
+											'<img src="../img/common/logo_gales.png">' +
+										'</div>' +
+										'<div class="col-xs-4 text-right">' +
+											'<a class="language_menu" href="#">IDIOMA: ESPAÑOL</a>' +
+										'</div>' +
+									'</div>' +
+								  '</div>');
+
+				// El link de seleccionar el idioma en el menú
+				$('.language_menu').on('click', function(e) {
+					e.preventDefault();
+					cargarSeleccionDeIdioma();
+				})
+			});
+
+			$('#menu').fadeIn('fast', function() {
+				$('.menu_link_toggle').on('click', function(e) {
+					e.preventDefault();
+
+					$(this).parents('.col-xs-12').find('ul').addClass('slideToggle')
+
+					$('#menu').find('ul').each(function() {
+						if ($(this).hasClass('slideToggle'))
+						{
+							$(this).slideToggle('fast', function() {
+								if ($(this).css('display') == 'none')
+								{
+									$(this).removeClass('slideToggle');
+								}
+							});
+						}
+					})
+					// $(this).parents('.col-xs-12').find('ul').slideToggle('fast');
+				})
+			});
+		}
+	});
 }
 
-//I'm only called when the file exists or has been downloaded.
-function appStart() {
-	alert('dentro del appStart - linea 185')
-	$status.innerHTML = "App ready!";
-			// alert(cordova.file.dataDirectory);
-			// document.getElementById("prueba").src=store + fileName;
-
-			// removeFile();
+function quitarMenu()
+{
+	$('#menu').fadeOut('fast', function() {
+		$('#menu').remove();
+	})
 }
 
-// remove file system entry
-        function removeFile() {
-	        alert('hola mundo');
-        	window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, onFileSystemSuccess, fail);
-        }
+/**
+ * [cargarSeleccionDeIdioma para mostrar el menú de idiomas]
+ * @return {null} 
+ */
+function cargarSeleccionDeIdioma()
+{
+	$('body').append('<div id="sel_language">' +
+						'<div class="close_btn">' +
+							'<img src="../../img/common/close.png" alt="Krion">' +
+						'</div>' +
+						'<div class="container">' +
+							'<div class="row">' +
+								'<div class="col-xs-12">' +
+									'<ul class="row text-center">' +
+										'<li class="col-xs-12">' +
+											'<h2>' +
+												'<a href="#" class="sel_language_link" data-lang="es">Español</a>' +
+											'</h2>' +
+										'</li>' +
+										'<li class="col-xs-12">' +
+											'<h2>' +
+												'<a href="#" class="sel_language_link" data-lang="en">English</a>' +
+											'</h2>' +
+										'</li>' +
+										'<li class="col-xs-12">' +
+											'<h2>' +
+												'<a href="#" class="sel_language_link" data-lang="fr">Français</a>' +
+											'</h2>' +
+										'</li>' +
+										'<li class="col-xs-12">' +
+											'<h2>' +
+												'<a href="#" class="sel_language_link" data-lang="it">Italiano</a>' +
+											'</h2>' +
+										'</li>' +
+										'<li class="col-xs-12">' +
+											'<h2>' +
+												'<a href="#" class="sel_language_link" data-lang="pt">Português</a>' +
+											'</h2>' +
+										'</li>' +
+										'<li class="col-xs-12">' +
+											'<h2>' +
+												'<a href="#" class="sel_language_link" data-lang="de">Deutsch</a>' +
+											'</h2>' +
+										'</li>' +
+										'<li class="col-xs-12">' +
+											'<h2>' +
+												'<a href="#" class="sel_language_link" data-lang="ru">Pусский</a>' +
+											'</h2>' +
+										'</li>' +
+										'<li class="col-xs-12">' +
+											'<h2>' +
+												'<a href="#" class="sel_language_link" data-lang="zh">汉语</a>' +
+											'</h2>' +
+										'</li>' +
+									'</ul>' +
+								'</div>' +
+							'</div>' +
+						'</div>' +
+					'</div>');
 
-        function onFileSystemSuccess(fileSystem) {
-	        // alert('name: ' + fileSystem.name);
-	        // alert('root.name: ' + fileSystem.root.name);
-	    }
+	var lang_page = setInterval(function() {
+		if ($('#sel_language').length)
+		{
+			$('#sel_language').fadeIn('fast');
 
-	    function fail(error) {
-	        alert(error.code);
-	    }
+			$('.sel_language_link').on('click', function(e) {
+				e.preventDefault();
 
-// function download(URL, Folder_Name, File_Name) {
-// //step to request a file system 
-//     window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, fileSystemSuccess, fileSystemFail);
+				if (localStorage.getItem("lang") != $(this).data('lang'))
+				{
+					console.log(localStorage.getItem("lang") + ' - ' + $(this).data('lang'))
+					localStorage.setItem("lang", $(this).data('lang'));
+				}
 
-// 	function fileSystemSuccess(fileSystem) {
-// 	    var download_link = encodeURI(URL);
-// 	    ext = download_link.substr(download_link.lastIndexOf('.') + 1); //Get extension of URL
+				// $('#sel_language').remove();
+				location.reload();
+			});
 
-// 	    var directoryEntry = fileSystem.root; // to get root path of directory
-// 	    directoryEntry.getDirectory(Folder_Name, { create: true, exclusive: false }, onDirectorySuccess, onDirectoryFail); // creating folder in sdcard
-// 	    var rootdir = fileSystem.root;
-// 	    var fp = rootdir.fullPath; // Returns Fulpath of local directory
-
-// 	    fp = fp + "/" + Folder_Name + "/" + File_Name + "." + ext; // fullpath and name of the file which we want to give
-// 	    // download function call
-// 	    filetransfer(download_link, fp);
-// 	}
-
-// 	function onDirectorySuccess(parent) {
-// 	    // Directory created successfuly
-// 	}
-
-// 	function onDirectoryFail(error) {
-// 	    //Error while creating directory
-// 	    alert("Unable to create new directory: " + error.code);
-// 	}
-
-// 	function fileSystemFail(evt) {
-// 	    //Unable to access file system
-// 	    alert(evt.target.error.code);
-// 	}
-// }
-
-// function filetransfer(download_link, fp) {
-// 	var fileTransfer = new FileTransfer();
-// 	// File download function with URL and local path
-// 	fileTransfer.download(download_link, fp,
-//             function (entry) {
-//                 alert("download complete: " + entry.fullPath);
-//             },
-// 			function (error) {
-// 				//Download abort errors or download failed errors
-// 				alert("download error source " + error.source);
-// 				//alert("download error target " + error.target);
-// 				//alert("upload error code" + error.code);
-// 			}
-//     );
-// }
+			$('.close_btn').on('click', function(e) {
+				e.preventDefault();
+				$('#sel_language').fadeOut('fast', function() {
+					$('#sel_language').remove();
+				})
+			})
+		}
+	})
+}
